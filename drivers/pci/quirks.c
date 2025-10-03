e746b3f4b6dcff177775eefa3062ebfdfec64627
Pepper Gray <111446242+peppergrayxyz@users.noreply.github.com>
2025-10-03 21:23:36 +0200

    [PATCH 2/2] ampere/arm64: Work around Ampere Altra erratum #82288  PCIE_65
    Altra's PCIe controller may generate incorrect addresses when receiving
    writes from the CPU with a discontiguous set of byte enables. Attempt to
    work around this by handing out Device-nGnRE maps instead of Normal
    Non-cacheable maps for PCIe memory areas.
    
    Signed-off-by: D Scott Phillips <scott@os.amperecomputing.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -6285,6 +6285,15 @@ DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_XILINX, 0x5021, of_pci_make_dev_node);
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_REDHAT, 0x0005, of_pci_make_dev_node);
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_EFAR, 0x9660, of_pci_make_dev_node);
 
+#ifdef CONFIG_ALTRA_ERRATUM_82288
+static void quirk_altra_erratum_82288(struct pci_dev *dev)
+{
+	pr_info_once("Write combining PCI maps disabled due to hardware erratum\n");
+	static_branch_enable(&have_altra_erratum_82288);
+}
+DECLARE_PCI_FIXUP_EARLY(PCI_VENDOR_ID_AMPERE, 0xe100, quirk_altra_erratum_82288);
+#endif
+
 /*
  * Devices known to require a longer delay before first config space access
  * after reset recovery or resume from D3cold:
